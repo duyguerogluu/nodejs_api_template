f6933fb7bedbeeb0640ee61155f3e92a7f714614
frknkrc44 <krc440002@gmail.com>
2023-12-05 15:29:22 +0300

    fix import error

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require("dotenv/config");
-const Product = require("../models/Product");
+const Product = require("../../models/Product");
 
 const router = express.Router();
 
